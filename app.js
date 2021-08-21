8391b1036e68b3b96be9867093fff2881cd8b083
Ramanath <ramanath@riselabs.co.uk>
2021-08-21 22:32:53 +0530

    cors added

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -22,6 +23,19 @@ const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
+
+app.use(cors())
+// Access-Control-Allow-Origin '*'
+// Subdomain - api.natours.com, frontend - natours.com
+// app.use(
+// 	cors({
+// 		origin: 'https://www.natours.com'
+// 	})
+// )
+
+// Instead of a specific route, enabling for all route for different kind of requests like PUT, PATCH, DELETE
+// app.options('/api/v1/tours/:id', cors())
+app.options('*', cors())
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(
